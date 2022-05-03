f2a66ff447996f49d7d34e9556660fdb535323f1
unknown <suresh.kumar8409@gmail.com>
2022-05-03 04:02:54 -0700

    otp-service-2

--- service/src/main/java/com/neom/fss/neompay/otp/utils/Utility.java
+++
@@ -1,9 +0,0 @@
-package com.neom.fss.neompay.otp.utils;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class Utility {
-
-}
